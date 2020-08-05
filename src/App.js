8372ff69cb3c53076b6f5cfe141c8fcecad4d00d
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-08-05 12:35:52 +0200

    word count

--- src/App.js
+++ src/App.js
@@ -4,31 +4,37 @@ import "./App.css";
 function App() {
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(5);
+  const [gameRunning, setgameRunning] = useState(false);
+  const [wordCount, setwordCount] = useState("");
 
   function handleChange(e) {
     const { value } = e.target;
     setText(value);
   }
   function countWords() {
-    let count = text.trim().split(" ").length;
-    return count.filter((word) => word !== "").length;
+    setgameRunning(true);
+    let count = text.trim().split(" ");
+    return setwordCount(count.filter((word) => word !== "").length);
   }
 
   useEffect(() => {
-    if (timeRemaining > 0) {
+    if (timeRemaining > 0 && gameRunning) {
       setTimeout(() => {
         setTimeRemaining((time) => time - 1);
       }, 1000);
+    } else if (timeRemaining === 0) {
+      countWords();
+      setgameRunning(false);
     }
-  }, [timeRemaining]);
+  }, [timeRemaining, gameRunning]);
 
   return (
     <div>
       <h1>How fast do you type?</h1>
       <textarea onChange={handleChange} value={text} />
-      <h4>Time remaining: ???</h4>
+      <h4>Time remaining: {timeRemaining}</h4>
       <button onClick={countWords}>Start</button>
-      <h1>Word count: {timeRemaining}</h1>
+      <h1>Word count:{wordCount} </h1>
     </div>
   );
 }
