5f8206154b488bf6e6782224af27d2ec91c01756
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-08-05 11:03:45 +0200

    counter & timer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+  function countWords() {
+    let count = text.trim().split(" ").length;
+    return count.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
+
   return (
     <div>
-      <h1>Speed typing</h1>
-      <textarea value="" />
-      <h4></h4>
-      <button>Start</button>
-      <h1></h1>
+      <h1>How fast do you type?</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time remaining: ???</h4>
+      <button onClick={countWords}>Start</button>
+      <h1>Word count: {timeRemaining}</h1>
     </div>
   );
 }
