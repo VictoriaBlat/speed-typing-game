7c5ef87471aab957caa06bca17f2acd7ba6e45d1
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-08-05 09:50:38 +0200

    app comp

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1>Speed typing</h1>
+      <textarea value="" />
+      <h4></h4>
+      <button>Start</button>
+      <h1></h1>
+    </div>
+  );
 }
 
 export default App;
