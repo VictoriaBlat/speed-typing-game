620b0805952ccd3e5a8b1fe3ffbdc10678d9bb3a
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-08-09 23:52:09 +0200

    fixed focus,restart game

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const STARTING_TIME = 5;
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [gameRunning, setgameRunning] = useState(false);
   const [wordCount, setwordCount] = useState("");
 
@@ -12,9 +13,13 @@ function App() {
     setText(value);
   }
   function countWords() {
-    setgameRunning(true);
     let count = text.trim().split(" ");
     return setwordCount(count.filter((word) => word !== "").length);
+  }
+  function startGame() {
+    setgameRunning(true);
+    setTimeRemaining(STARTING_TIME);
+    setText("");
   }
 
   useEffect(() => {
@@ -31,9 +36,11 @@ function App() {
   return (
     <div>
       <h1>How fast do you type?</h1>
-      <textarea onChange={handleChange} value={text} />
+      <textarea onChange={handleChange} value={text} disabled={!gameRunning} />
       <h4>Time remaining: {timeRemaining}</h4>
-      <button onClick={countWords}>Start</button>
+      <button disabled={gameRunning} onClick={startGame}>
+        Start
+      </button>
       <h1>Word count:{wordCount} </h1>
     </div>
   );
